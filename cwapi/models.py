875fb2aab81b4c451b692fdfda572a2d6eec473d
jmagan01@dcs.bkk.ac.uk <jmagan01@dcs.bbk.ac.uk>
2020-04-18 18:25:46 +0000

    Implement validators for all models, Run TC 10

--- cwapi/models.py
+++ cwapi/models.py
@@ -1,30 +1,85 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+
+# Libraries for data validation
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+# Other useful libraries
 from datetime import datetime
 
 # Create your models here.
 class Auction(models.Model):
+	"""
+	This model holds the key data of the item to be auctioned.
+	All items are first created in this model/table.
+	"""
 
-	item_name = models.CharField(
-		max_length = 100,
-		verbose_name = 'Item Title')
-	ask_price = models.PositiveIntegerField(verbose_name = 'Starting Price (£)')
-	seller = models.CharField(max_length = 25) #? Get it from oAuth
-	posted_timedate = models.DateTimeField(
-		auto_now_add = True, 
-		editable = False)
-	expiration_timedate = models.DateTimeField()
-	#is_active = models.BooleanField()
-	auction_status = models.CharField(
-		max_length = 25, 
-		default = "Open to offers")
-	auction_winner = models.CharField(
-		max_length = 25,
-		editable = False,
-		blank = False,
-		null = True,)
+	# Validators 
+	def is_in_the_future(value):
+		now = datetime.utcnow()
+		if value <= now:
+			raise ValidationError(_('%(value)s must be a future date and time'), 
+				params = {'value': value},)
+			
+	def is_positive(value):
+		if value <= 0:
+			raise ValidationError(_('%(value) must be a positive number'), 
+				params = {'value': value},)
 	
+	# Model Fields
+	item_name = models.CharField(
+		# Explicit validation, unique, not null, not blank
+		unique=True, #unique implies the creation of an index.
+		max_length=100,
+		verbose_name='Item Title')
+		
+	ask_price = models.DecimalField(
+		max_digits=12,
+		decimal_places=2,
+		# Explicit validation, positive integer, not null, not blank
+		blank=False,
+		null=False,
+		verbose_name='Starting Price (£)',
+		validators = [is_positive])
+		
+	seller = models.CharField(
+		# Explicit validation, not null, not blank
+		blank=False,
+		null=False,
+		max_length=25) #? Get it from oAuth
+		
+	auction_status = models.CharField(
+		# The field will not be displayed in the admin or any other ModelForm
+		editable=False,
+		max_length=25,
+		default="Open to offers")
+
+	auction_winner = models.CharField(
+		# Explicit validation, not null, not blank
+		blank=False,
+		null=False,
+		max_length=25,
+		default="To be confirmed")
+	
+	posted_timedate = models.DateTimeField(
+		# Automatically set the field to now when the object is first created
+		# The field will not be displayed in the admin or any other ModelForm
+		auto_now_add=True,
+		editable=False)
+	
+	expiration_timedate = models.DateTimeField(
+		# Use a custom validator
+		validators = [is_in_the_future])
+		
+	last_update = models.DateTimeField(
+		# Automatically set the field to now every time the object is saved
+		# The field will not be displayed in the admin or any other ModelForm
+		auto_now=True,
+		editable=False)
+
 	def get_time_delta(self):
 		return self.expiration_timedate - datetime.now()
 	
@@ -42,12 +97,6 @@ class Auction(models.Model):
 			hour, minutes = divmod(totalmin, 60)
 			remaining_time = '{} Days, {} Hours, {} Minutes, {} Seconds'.format(td.days,hour,minutes,seconds)
 		return remaining_time
-	
-	# def	update_auction(self, bid_price, bidder_name):
-		# if bid_price > self.current_bid_price:
-			# self.best_bidder_name = bidder_name 
-			# self.current_bid_price = bid_price
-		# return self.best_bidder_name, self.current_bid_price
 	
 	def close_auction(self):
 		seld.auction_status = "Completed"
@@ -71,32 +120,62 @@ class Auction(models.Model):
 	def __str__(self):
 		return 'Auction %s - %s' % (self.id, self.item_name)
 
-
 class Bid(models.Model):
+
+	def is_positive(value):
+		if value <= 0:
+			raise ValidationError(_('%(value) must be a positive number'), 
+				params = {'value': value},)
+	
+	# Custom validators			
+	# def is_not_seller(self, data):
+		# """
+        # Check that bidder is not the seller
+        # """
+		# if self.bidder == "mary":
+			# raise ValidationError("The seller cannot bid for its own item")
+
+	# def	update_auction(self, bid_price, bidder_name):
+		# if bid_price > self.current_bid_price:
+			# self.best_bidder_name = bidder_name 
+			# self.current_bid_price = bid_price
+		# return self.best_bidder_name, self.current_bid_price
+	
+	# Model fields
 	auction_id = models.ForeignKey(
 		'Auction', 
-		on_delete = models.CASCADE)
-	bidder_name = models.CharField(max_length = 25) #? Get it from oAuth
-	bid_price = models.DecimalField(max_digits = 6, decimal_places = 2, verbose_name ='Bid')
-	bid_timestamp = models.DateTimeField(auto_now_add = True, blank = False, editable = False)
-	
-	def save(self, *args, **kwargs):
-		if Bid.auction_id.is_active:
-			#Save bid in database
-			super(Bid, self).save(*args, **kwargs) #Real save
-		else: 
-			raise Exception('The auction is closed. The following bid has not been accepted: {}'.format(self.bid_price))
+		related_name='bids',
+		on_delete=models.CASCADE)
 		
+	bidder = models.CharField(
+		# Explicit validation, not null, not blank
+		blank=False,
+		null=False,
+		max_length=25,
+		verbose_name='Bidder name',) #? Get it from oAuth
+
+	bid_price = models.DecimalField(
+		max_digits=12,
+		decimal_places=2,
+		verbose_name='Bid',
+		validators = [is_positive,]) #validate vs current_bid_price
+
+	bid_timestamp = models.DateTimeField(
+		auto_now_add=True,
+		blank=False,)
 	
 	def __str__(self):
-		return "%s %s %s" % (self.id, self.bidder, bid_price)
-		
-		
+		return 'Bid %s - %s - £ %s - %s' % (self.id, self.bidder, self.bid_price, self.bid_timestamp)
 		
 class ItemDetail(models.Model):
+	"""
+	This model holds additional details about the item to be auctioned.
+	None of the fields in this class are essential to complete the auction process.
+	"""
 	CONDITION_TYPE = (
 		('N', 'New'),
 		('U', 'Used'))
+	
 	CATEGORIES = (
 		('A&C', 'Antiques & Collectibles'),
 		('AUT', 'Automotive'),
@@ -111,20 +190,34 @@ class ItemDetail(models.Model):
 		('S&V', 'Software & Videogames'),
 		('T&E', 'Tools & Equipment'),
 		('TOY', 'Toys'))
+	
 	auction_id = models.OneToOneField(
 		'Auction',
-		 related_name = 'items', 
+		#primary_key=True, #django won’t add the automatic id column.
+		related_name='item_details',
 		on_delete = models.CASCADE)
-	item_description = models.TextField(verbose_name='Item Description')
-	item_quantity = models.IntegerField(verbose_name='Quantity')
+	
+	item_description = models.TextField(
+		# Field is optional
+		blank=True,
+		max_length=500,
+		verbose_name='Item Description')
+
+	item_quantity = models.PositiveIntegerField(
+		blank=False,
+		default=1,		
+		verbose_name='Quantity')
+
 	item_category = models.CharField(
-		max_length = 3,
-		choices = CATEGORIES,
-		verbose_name ='Item Category')
+		max_length=3,
+		choices=CATEGORIES,
+		verbose_name='Item Category')
+
 	item_condition = models.CharField(
-		max_length = 1,
-		choices = CONDITION_TYPE,
-		verbose_name = 'Condition of the Item')
+		max_length=1,
+		choices=CONDITION_TYPE,
+		verbose_name='Condition of the Item')
 
 	def __str__(self):
 		return 'Item %s - %s' % (self.id, self.auction_id.item_name)
+
--- cwapi/serializers.py
+++ cwapi/serializers.py
@@ -1,41 +1,86 @@
 from rest_framework import serializers
 from . models import *
+from datetime import datetime
 
-class ItemDetailSerializer(serializers.HyperlinkedModelSerializer):
+class ItemDetailSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ItemDetail
 		fields = [
-			'url','id',
+			'url', 'id',
 			'auction_id',
 			'item_description',
 			'item_quantity',
 			'item_category',
 			'item_condition',
 			]
+
+class AuctionDataSerializer(serializers.ModelSerializer):
+	class Meta:
+			model = Auction
+			fields = [
+				'url', 
+				'id',
+				'is_active',
+				'seller',
+				]
+	
+class BidSerializer(serializers.ModelSerializer):
+	auctiondata = AuctionDataSerializer(
+		source='bids',
+		required=False, #Non-compulsory information
+		read_only=True
+		)
+
+	class Meta:
+		model = Bid
+		fields = [
+			'url','id',
+			'auction_id',
+			'auctiondata',
+			'bid_price',
+			'bidder',
+			]
+		#read_only_fields = [
+		#	'auction_data',
+		#	]
 		
+	# def validate_date_of_birth(self, dob):
+        # today = date.today()
+        # age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        # if (not(20 < age < 30)):
+            # raise serializers.ValidationError("You are no eligible for the job")
+        # return dob
+
 class AuctionSerializer(serializers.HyperlinkedModelSerializer):
-	item_details = ItemDetailSerializer(source='items', read_only=True)
+	item_detail = ItemDetailSerializer(
+		source='item_details',
+		required=False, #Non-compulsory information
+		read_only=True)
+		
+	bid_details = BidSerializer(
+		source='bids',
+		many=True,
+		required=False, #Non-compulsory information
+		read_only=True)
 
 	class Meta:
 		model = Auction
 		fields = [
 			'url','id',
 			'item_name',
-			'item_details', #nested serializer
+			'item_detail', #Nested serializer
 			'is_active',
 			'auction_status',
-			'ask_price',
 			'time_left',
 			'expiration_timedate',
 			'seller',
+			'ask_price',
+			'auction_winner',
+			'bid_details',
 			]
-
-class BidSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Bid
-		fields = [
-			'url','id',
-			'auction_id',
-			'bidder_name',
-			'bid_price',
+		read_only_fields = [
+			'is_active',
+			'auction_status',
+			'time_left',
+			'auction_winner',
 			]
